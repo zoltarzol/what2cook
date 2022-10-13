739b483104e8f6027515d064c02167b2ab9c5bb3
Cedric Duroisin <duroisin@gmail.com>
2022-10-13 13:57:58 +0200

    initial sync between VS Code and GitHub

---
+++ what2cook/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-20z)s66v%0r3#y3xa0snz7jv#qu$cbpg(zifnt+hdz07dei@^q"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
