1f0a07300db23b44438b1556009154bfae8756ad
Cedric Duroisin <duroisin@gmail.com>
2022-10-13 17:00:18 +0200

    homepage - travail en cours - premier commit

--- home/models.py
+++ home/models.py
@@ -1,7 +1,25 @@
 from django.db import models
-
 from wagtail.models import Page
-
+from wagtail.admin.edit_handlers import FieldPanel, PageChooserPanel
+from wagtail.core.fields import StreamField
 
 class HomePage(Page):
-    pass
+    subtitle_text = models.CharField(
+        max_length = 50,
+        blank = True,
+        help_text = "Homepage subtitle"
+    )
+
+    background_image = models.ForeignKey(
+        'wagtailimages.Image',
+        blank=True,
+        null=True,
+        related_name="+",
+        help_text="Background image",
+        on_delete = models.SET_NULL
+    )
+
+    content_panels = Page.content_panels + [
+        FieldPanel("subtitle_text"),
+        FieldPanel("background_image"),
+    ]
